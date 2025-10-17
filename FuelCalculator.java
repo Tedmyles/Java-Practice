5fd24284638797239af4c866ec05d6aebc721ed7
Tedmyles <mylesteddy7@gmail.com.com>
2025-10-17 17:26:15 +0300

    java-practice

---
+++ FuelCalculator.java
@@ -0,0 +1,29 @@
+// A vehicle needs 10 times the amount of fuel than the distance it travels. However, it must always carry a minimum of 100 fuel before setting off.
+
+// Create a function which calculates the amount of fuel it needs, given the distance.
+
+// Examples
+// calculateFuel(15) ➞ 150
+
+// calculateFuel(23.5) ➞ 235
+
+// calculateFuel(3) ➞ 100
+// Notes
+// Distance will be a number greater than zero.
+// Return 100 if the calculated fuel turns out to be less than 100.
+
+public class FuelCalculator {
+    
+    public static int calculateFuel(double distance){
+        double fuelNeeded = distance * 10;
+        return (int) Math.max(fuelNeeded, 100);
+
+
+    }
+    public static void main(String[] args) {
+        System.out.println(calculateFuel(15));    // ➞ 150
+        System.out.println(calculateFuel(23.5));  // ➞ 235
+        System.out.println(calculateFuel(3));     // ➞ 100
+    }
+
+}
